3dce92e48c6c71494c382213a1b5cff505987cc2
Swamy <thudumswamy786@gmail.com>
2022-09-28 14:06:55 +0530

    added production file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndPoints:{
+    addJobPost:"https://spring-find-dev-rest-apis.herokuapp.com/api/jobs/add-job-post",
+    getAllPost:'https://spring-find-dev-rest-apis.herokuapp.com/api/jobs/',
+    updateJobPost:'https://spring-find-dev-rest-apis.herokuapp.com/api/jobs/job-post/edit/',
+    deleteJobPost:'https://spring-find-dev-rest-apis.herokuapp.com/api/jobs/delete/',
+    searchPost:'https://spring-find-dev-rest-apis.herokuapp.com/api/jobs/role?search=developer'
+  }
 };
